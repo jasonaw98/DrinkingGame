6054052a3b83e180232f4e18ef1f3483bf909c75
Jason <jason.aw@attspace.ventures>
2023-08-22 23:30:01 +0800

    Card component

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,31 @@
+'use client'
+import { useState } from 'react';
+
 import CardComponent from '@/components/CardComponent'
 
 export default function Home() {
+  const cardsData = [
+    { id: 1, text: 'Drink 1 sips' },
+    { id: 2, text: 'Drink 2 sips' },
+    { id: 3, text: 'Drink 3 sips' },
+    { id: 4, text: 'Drink 4 sips' },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-0">
-      {/* <div className='grid grid-cols-2 gap-20 place-content-center h-48'> */}
-      <div className='grid grid-cols-2 gap-7 place-content-center h-48 gap-y-14'>
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
+    <main className="">
+      <div className='flex justify-center text-[32px] pt-16 mb-[-6rem] font-extrabold text-gray-100'>
+      <h1 className='drop-shadow-md'>🍻 Drinking Game 🥂</h1>
       </div>
+      <div className='flex min-h-screen flex-col items-center justify-center p-0'>
+      <div className='grid grid-cols-2 gap-7 place-content-center h-48 gap-y-10'>
+      {cardsData.map((card) => (
+        <CardComponent
+        key={card.id}
+        dare={card.text}
+        />
+        ))}
+      </div>
+        </div>
     </main>
   )
 }
--- components/CardComponent.jsx
+++ components/CardComponent.jsx
@@ -1,12 +1,33 @@
+'use client'
+import { useState } from 'react';
 
-const CardComponent = () => {
+const CardComponent = ({dare}) => {
+    const [flipped, setFlipped] = useState(false);
+
+    const flipCard = () => {
+        setFlipped(!flipped);
+      };
+
   return (
     <>
-    <div className="flex flex-col justify-center items-center bg-slate-900 w-[156px] border-l-4 border-slate-700 rounded-[1rem] min-h-[207px] p-4 drop-shadow-lg">
-        <div className="text-center font-bold text-white text-[23px] mb-2">Do or Drink</div>
-      <div className="border-b-2 border-white w-12"></div>
+   <div
+      className={`card ${flipped ? 'flipped' : ''}`}
+      onClick={flipCard}
+    >
+      <div className="card-front">
+        <div className="flex flex-col justify-center items-center bg-slate-900 w-[156px] border-l-4 border-b-4 border-slate-700 rounded-[1rem] min-h-[207px] p-4 drop-shadow-lg">
+          <div className="text-center font-bold text-white text-[20px] mb-2">Do or Drink</div>
+          <div className="border-b-2 border-white w-16"></div>
+        </div>
+      </div>
+
+      <div className="card-back">
+        <div className="flex flex-col justify-center items-center bg-slate-900 w-[156px] border-l-4 border-b-4 border-t-4 border-r-4 border-slate-700 rounded-[1rem] min-h-[207px] p-4 drop-shadow-lg">
+          <div className="text-center font-bold text-white text-[23px] mb-2">{dare}</div>
+          <div className="border-b-2 border-white w-16"></div>
+        </div>
+      </div>
     </div>
-    
     </>
   )
 }
