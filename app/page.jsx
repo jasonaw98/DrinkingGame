eb944aa8578c1bfed464b3b82b4ce3978f625007
Jason <jason.aw@attspace.ventures>
2023-09-04 18:53:59 +0800

    PWA

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,9 @@
-'use client'
-import { useState, useEffect } from 'react';
-import CardComponent from '@/components/CardComponent'
-import Link from 'next/link';
+"use client";
+import { useState, useEffect } from "react";
+import CardComponent from "@/components/CardComponent";
+import Link from "next/link";
 
 export default function Home() {
-
   const [cardsData, setCardsData] = useState([]);
   const [cardsDataSet, setCardsDataSet] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -13,33 +12,35 @@ export default function Home() {
     const shuffledArray = array.slice().sort(() => 0.5 - Math.random());
     return shuffledArray.slice(0, num);
   }
-  
+
   // Function to populate cardsSet with random data
   function refreshCardsDataSet() {
     if (cardsData.length > 0) {
       const randomCards = getRandomElements(cardsData, 4);
-  
+
       const newCardsSet = randomCards.map((card, index) => ({
         id: index + 1,
         category: card.category,
         dare: card.dare,
         author: card.author,
       }));
-  
+
       setCardsDataSet(newCardsSet);
-      console.log("New Set",newCardsSet);
+      console.log("New Set", newCardsSet);
     }
   }
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbwqLjXZKiay7qzp2CIFWDUbSHgudgZWY019cs1FQK08KFQj-zhpRyC27_0ItbayOU1Zyw/exec');
+        const response = await fetch(
+          "https://script.google.com/macros/s/AKfycbwqLjXZKiay7qzp2CIFWDUbSHgudgZWY019cs1FQK08KFQj-zhpRyC27_0ItbayOU1Zyw/exec"
+        );
         const fetchedData = await response.json();
         // console.log(fetchedData);
         setCardsData(fetchedData);
       } catch (error) {
-        alert('Error fetching data:', error);
+        alert("Error fetching data:", error);
       } finally {
         setIsLoading(false);
       }
@@ -51,7 +52,7 @@ export default function Home() {
   useEffect(() => {
     if (cardsData.length > 0) {
       const randomCards = getRandomElements(cardsData, 4);
-  
+
       const cardsSet = randomCards.map((card, index) => ({
         id: index + 1,
         category: card.category,
@@ -60,7 +61,7 @@ export default function Home() {
       }));
       setCardsDataSet(cardsSet);
     }
-  }, [cardsData])
+  }, [cardsData]);
 
   const handleCardFlippedBack = (flippedState) => {
     if (flippedState) {
@@ -70,36 +71,59 @@ export default function Home() {
 
   return (
     <main className="">
-      <div className='flex justify-center text-[32px] pt-16 mb-[-6rem] font-extrabold text-gray-100'>
-      <h1 className='drop-shadow-md'>🍻 Sippin ? Skippin 🍻</h1>
+      <div className="flex justify-center text-[32px] pt-16 mb-[-6rem] font-extrabold text-gray-100">
+        <h1 className="drop-shadow-md">🍻 Sippin ? Skippin 🍻</h1>
       </div>
       {isLoading ? (
-          <div className="flex items-center justify-center h-screen">
-              <div className="pt-8" role="status">
-                  <svg aria-hidden="true" className="w-16 h-16 pb-4 text-gray-200 animate-spin dark:text-gray-600 fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
-                  <span className="sr-only">Loading...</span>
+        <div className="flex items-center justify-center h-screen">
+          <div
+            aria-label="Orange and tan hamster running in a metal wheel"
+            role="img"
+            className="wheel-and-hamster"
+          >
+            <div className="wheel"></div>
+            <div className="hamster">
+              <div className="hamster__body">
+                <div className="hamster__head">
+                  <div className="hamster__ear"></div>
+                  <div className="hamster__eye"></div>
+                  <div className="hamster__nose"></div>
+                </div>
+                <div className="hamster__limb hamster__limb--fr"></div>
+                <div className="hamster__limb hamster__limb--fl"></div>
+                <div className="hamster__limb hamster__limb--br"></div>
+                <div className="hamster__limb hamster__limb--bl"></div>
+                <div className="hamster__tail"></div>
               </div>
+            </div>
+            <div className="spoke"></div>
           </div>
-      ):(
-        <div className='flex min-h-screen flex-col items-center justify-center p-0'>
-        <div className='grid grid-cols-2 gap-7 place-content-center h-48 gap-y-10'>
-        {cardsDataSet.map((card) => (
-          <CardComponent
-          key={card.id}
-          category={card.category}
-          dare={card.dare}
-          author={card.author}
-          onCardFlippedBack={handleCardFlippedBack}
-          />
-          ))}
+        </div>
+      ) : (
+        <div className="flex min-h-screen flex-col items-center justify-center p-0">
+          <div className="grid grid-cols-2 gap-7 place-content-center h-48 gap-y-10">
+            {cardsDataSet.map((card) => (
+              <CardComponent
+                key={card.id}
+                category={card.category}
+                dare={card.dare}
+                author={card.author}
+                onCardFlippedBack={handleCardFlippedBack}
+              />
+            ))}
           </div>
-          </div>
-          )}
-           <div className='flex justify-center text-[32px] pt-0 mt-[-6rem] font-extrabold text-gray-100'>
-            <Link href="https://forms.gle/JxUZw4R89HEe6CSd6">
-           <button type="button" class="text-white bg-gradient-to-r from-indigo-400 to-blue-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-md">Add Yours</button>
-            </Link>
-           </div>  
+        </div>
+      )}
+      <div className="flex justify-center text-[32px] pt-0 mt-[-6rem] font-extrabold text-gray-100">
+        <Link href="https://forms.gle/JxUZw4R89HEe6CSd6">
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-indigo-400 to-blue-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-md"
+          >
+            Add Yours
+          </button>
+        </Link>
+      </div>
     </main>
-  )
+  );
 }
